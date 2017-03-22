daaf2a05db7461d8361e28d254277a6a0c4c0273
Christopher Speller <crspeller@gmail.com>
2017-03-22 11:04:23 -0400

    Inital commit

---
+++ matterbuild.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2015 Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
+package main
+
+import "github.com/mattermost/matterbuild/server"
+
+func main() {
+	server.Start()
+}
---
+++ server/server.go
@@ -0,0 +1,7 @@
+// Copyright (c) 2017 Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
+package server
+
+func Start() {
+}
