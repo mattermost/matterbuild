31f0e8db02f27f43d39ae534548adc0ef8c3042e
Jesse Hallam <jesse.hallam@gmail.com>
2020-05-11 11:53:06 -0300

    remove deprecated tests for plugin tag regex

--- server/server_test.go
+++ server/server_test.go
@@ -4,31 +4,10 @@
 package server
 
 import (
-	"regexp"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
-
-func TestPluginTagRegex(t *testing.T) {
-	t.Run("valid tags", func(t *testing.T) {
-		require.Regexp(t, regexp.MustCompile(pluginTagRegex), "v5.2.3")
-		require.Regexp(t, regexp.MustCompile(pluginTagRegex), "v55.52.53")
-		require.Regexp(t, regexp.MustCompile(pluginTagRegex), "v552.522.532")
-	})
-
-	t.Run("invalid tags", func(t *testing.T) {
-		require.NotRegexp(t, regexp.MustCompile(pluginTagRegex), "5.2.3")
-		require.NotRegexp(t, regexp.MustCompile(pluginTagRegex), "5a2.3")
-		require.NotRegexp(t, regexp.MustCompile(pluginTagRegex), "5.2a3")
-		require.NotRegexp(t, regexp.MustCompile(pluginTagRegex), " v 5 . 2 . 3 ")
-		require.NotRegexp(t, regexp.MustCompile(pluginTagRegex), "v55..52.53")
-		require.NotRegexp(t, regexp.MustCompile(pluginTagRegex), "v552.522..532")
-		require.NotRegexp(t, regexp.MustCompile(pluginTagRegex), "zzzv552.522.532")
-		require.NotRegexp(t, regexp.MustCompile(pluginTagRegex), "123zzzv552.522.532")
-		require.NotRegexp(t, regexp.MustCompile(pluginTagRegex), "    v552.522.532    ")
-	})
-}
 
 func TestCheckSlashPermissions(t *testing.T) {
 	t.Run("allowed commands", func(t *testing.T) {
